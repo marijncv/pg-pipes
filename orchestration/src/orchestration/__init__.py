8e2a74f7f1f8b1e308c0d74f43d3f3b7f71b75b6
Marijn Valk <marijncv@hotmail.com>
2024-10-20 11:22:00 +0200

    ruff

--- orchestration/src/orchestration/__init__.py
+++ orchestration/src/orchestration/__init__.py
@@ -12,22 +12,22 @@ from dagster import (
 )
 import psycopg2
 
+
 @asset
-def pg_pipes_asset(context: AssetExecutionContext) -> Iterator[Union[MaterializeResult, AssetCheckResult]]:
+def pg_pipes_asset(
+    context: AssetExecutionContext,
+) -> Iterator[Union[MaterializeResult, AssetCheckResult]]:
     with open_pipes_session(
         context=context,
         context_injector=PipesTempFileContextInjector(),
         message_reader=PipesTempFileMessageReader(),
     ) as pipes_session:
-
         context_path = pipes_session.context_injector_params["path"]
         messages_path = pipes_session.message_reader_params["path"]
         query = "SELECT * FROM pg_tables"
 
         conn = psycopg2.connect(
-            database="pg_pipes",
-            host=Path.home() / ".pgrx",
-            port=28813
+            database="pg_pipes", host=Path.home() / ".pgrx", port=28813
         )
 
         cursor = conn.cursor()
@@ -36,6 +36,8 @@ def pg_pipes_asset(context: AssetExecutionContext) -> Iterator[Union[Materialize
         cursor.execute("drop extension if exists pg_pipes; create extension pg_pipes;")
 
         # start the pipes_session in postgres
-        cursor.execute(f"select pipes_session('{context_path}', '{messages_path}', '{query}');")
+        cursor.execute(
+            f"select pipes_session('{context_path}', '{messages_path}', '{query}');"
+        )
 
     yield from pipes_session.get_results()
